020d9bab87e7a12114d5fac9396cd6dd7ee18828
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-26 10:27:16 -0700

    assign roles to users

--- src/WorldTravelBlog/Controllers/RolesController.cs
+++ src/WorldTravelBlog/Controllers/RolesController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Http.Internal;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
 using WorldTravelBlog.Models;
 
 namespace WorldTravelBlog.Controllers
@@ -13,9 +15,11 @@ namespace WorldTravelBlog.Controllers
     public class RolesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public RolesController(ApplicationDbContext context)
+        public RolesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
 
@@ -48,6 +52,40 @@ namespace WorldTravelBlog.Controllers
                 _context.SaveChanges();
             }
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Assign()
+        {
+            ViewBag.Users = new SelectList(_userManager.Users.ToList());
+
+            return View();
+        }
+
+        
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult GetRoles(string user)
+        {
+            var usera = GetUser(user);
+            ViewBag.User = usera;
+            ViewBag.RolesForThisUser = _userManager.GetRolesAsync(usera).Result;
+
+            ViewBag.Users = new SelectList(_userManager.Users.ToList());
+            ViewBag.Roles = new SelectList(_context.Roles.ToList());
+            return View("Assign");
+        }
+
+        public IActionResult AddToUser(string username, string roleName)
+        {
+            var user = GetUser(username);
+            var thing = _userManager.AddToRoleAsync(user, roleName).Result;
+            return RedirectToAction("Assign");
+        }
+
+        public ApplicationUser GetUser(string username)
+        {
+            return _userManager.Users.FirstOrDefault(m => m.UserName == username);
         }
     }
 }
